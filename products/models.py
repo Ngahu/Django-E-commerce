021d5289967ee092fc7f2d64b8f9ebc7d778e96a
Ngahu <jamaalaraheem@gmail.com>
2018-11-06 14:53:36 +0300

    creating of the active model field in the product model and also creating of custom querysets for the product model

--- products/models.py
+++ products/models.py
@@ -20,7 +20,25 @@ def upload_image_path(instance,filename):
 
 
 
+class ProductQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(active=True)
 
+    def featured(self):
+        return self.filter(featured=True,active=True)
+
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return ProductQuerySet(self.model,using=self._db)
+    def all(self):
+        return self.get_queryset().active()
+
+    def features(self):
+        return self.get_queryset().featured()
+
+    def get_by_id(self,id):
+        return self.get_queryset().filter(id=id)
 
 
 class Product(models.Model):
@@ -30,6 +48,10 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(decimal_places=2,max_digits=20)
     image = models.ImageField(upload_to=upload_image_path,blank=True, null=True)
+    featured = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
+
+    objects = ProductManager()
 
 
     def __str__(self):
