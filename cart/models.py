9945a0e848df24eaf0beacd9f08ebf01b8fcd5ab
Ngahu <jamaalaraheem@gmail.com>
2018-11-07 00:38:45 +0300

    Creating of the new_or_get model manager that will carry all the business logic of getting and creating of new carts.

--- cart/models.py
+++ cart/models.py
@@ -8,6 +8,26 @@ User = settings.AUTH_USER_MODEL
 
 
 class CartManager(models.Manager):
+
+    def new_or_get(self,request):
+        cart_id = request.session.get("cart_id", None)
+        qs = Cart.objects.filter(id=cart_id)
+        if qs.count() ==1:
+            new_obj = False
+            cart_obj = qs.first()
+            if request.user.is_authenticated() and cart_obj.user is None:
+                cart_obj.user = request.user
+                cart_obj.save()
+        else:
+            #create a new cart
+            cart_obj = Cart.objects.new(user=request.user)
+            new_obj = True
+            request.session['cart_id'] = cart_obj.id
+        
+        return cart_obj,new_obj
+
+
+
     def new(self,user=None,products=None):
         """
         Description:Create a new cart.\n
--- cart/views.py
+++ cart/views.py
@@ -4,19 +4,7 @@ from django.shortcuts import render
 from .models import Cart
 
 def cart_home(request):
-    cart_id = request.session.get("cart_id", None)
-    qs = Cart.objects.filter(id=cart_id)
-    if qs.count() ==1:
-        print("Cart exists")
-        cart_obj = qs.first()
-        if request.user.is_authenticated() and cart_obj.user is None:
-            cart_obj.user = request.user
-            cart_obj.save()
-    else:
-        #create a new cart
-        cart_obj = Cart.objects.new(user=request.user)
-        request.session['cart_id'] = cart_obj.id
-
+    cart_obj = Cart.objects.new_or_get(request)
     template_name = 'cart/cart.html'
     context = {}
     return render(request,template_name,context)
