9609f18eccbf4f0631ba99fd649fc6bcb56a6fec
Ngahu <jamaalaraheem@gmail.com>
2018-11-06 23:13:03 +0300

    Creating of the cart model

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
+
+from products.models  import Product
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
+
+
+
+class Cart(models.Model):
+    """
+    Description:Store a cart belonging to a user.\n
+    """
+    user = models.ForeignKey(User,blank=True, null=True)
+    products = models.ManyToManyField(Product,blank=True)
+    total = models.DecimalField(default=0.00,max_digits=100,decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.id)
--- cart/views.py
+++ cart/views.py
@@ -4,6 +4,16 @@ from django.shortcuts import render
 
 
 def cart_home(request):
+    cart_id = request.session.get("cart_id", None)
+    if cart_id is None:
+        print("Cart is none.Create one")
+        pass
+    else:
+        print("Cart exists")
+
+
+
+
     template_name = 'cart/cart.html'
     context = {}
     return render(request,template_name,context)
