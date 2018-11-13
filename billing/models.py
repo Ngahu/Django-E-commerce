cbb7efc5747f64f1e9c2bac678bd2c4b72a80930
Ngahu <jamaalaraheem@gmail.com>
2018-11-13 16:53:38 +0300

    Creating of the billing profile model

--- billing/models.py
+++ billing/models.py
@@ -1,3 +1,32 @@
+from django.conf import settings
 from django.db import models
-
+from django.db.models.signals import post_save
 # Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+
+
+class BillingProfile(models.Model):
+    """
+    """
+    user = models.OneToOneField(User,blank=True, null=True)
+    email = models.EmailField()
+    active = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.email
+
+
+
+
+
+def user_created_receiver(sender,instance,created,*args,**kwargs):
+    if created and instance.email:
+        BillingProfile.objects.get_or_create(user=instance,email=instance.email)
+
+
+post_save.connect(user_created_receiver,sender=User)    
