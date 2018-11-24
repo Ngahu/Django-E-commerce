689da0d2a4aa753a779041a4e3044a73ba5ef03f
Ngahu <jamaalaraheem@gmail.com>
2018-11-24 06:50:09 +0300

    Creating of the custom user model manager

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,47 @@ from django.conf import settings
 
 
 
+class UserManager(BaseUserManager):
+    def create_user(self,email,password=None,is_active=True,is_staff=False,is_admin=False):
+        if not email:
+            raise ValueError("Users must have an email address")
+        if not password:
+            raise ValueError("Users must have a password")
+        user_obj = self.model(
+            email=self.normalize_email(email)
+        )
+        user_obj.set_password(password)
+        user_obj.staff=is_staff
+        user_obj.admin = is_admin
+        user_obj.active = is_active
+        user_obj.save(using=self._db)
+        return user_obj
+
+    
+
+    def create_staffuser(self,email,password=None):
+        user = self.create_user(
+            email,
+            password=password,
+            is_staff=True
+        )
+        return user
+
+    
+    def create_superuser(self,email,password=None):
+        user = self.create_user(
+            email,
+            password=password,
+            is_staff=True,
+            is_admin=True
+        )
+        return user
+
+
+
+
+
+
 
 
 class User(AbstractBaseUser):
@@ -20,6 +61,8 @@ class User(AbstractBaseUser):
 
 
     USERNAME_FIELD = 'email'
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
